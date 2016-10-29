bddabb62b9e369647f3280344bc2c55093c042ac
Noah <noahzgames@gmail.com>
2016-10-29 16:27:12 +0200

    Stop snooping

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-		return "Test"
+	return render_template('index.html')
+
+@app.route('/')
+def cookies():
+	return 'Cookies and cream'
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
